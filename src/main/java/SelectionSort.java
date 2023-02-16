8e7066007df34717c8e1b57e28ad540528822787
Maks Burenko <Cherniymaks@yandex.ru>
2023-02-16 16:55:38 +0300

    Initial commit

---
+++ src/main/java/SelectionSort.java
@@ -0,0 +1,14 @@
+public class SelectionSort {
+    public static int[] sort(int[] items){
+        for(int i = 0; i<items.length; i++){
+            int temp = items[i];
+            for(int j = i+1; j<items.length; j++){
+                if(items[j] < items[i]){
+                    items[i] = items[j];
+                    items[j] = temp;
+                }
+            }
+        }
+        return items;
+    }
+}
