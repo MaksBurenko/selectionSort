a46f69f3894fb7331d62f08c00cf64f2cd3f0a65
Maks Burenko <Cherniymaks@yandex.ru>
2023-02-22 14:55:22 +0300

    123

--- src/main/java/SelectionSort.java
+++ src/main/java/SelectionSort.java
@@ -1,20 +1,19 @@
 public class SelectionSort {
 
-    public static int[] sort(int[] items){
+    public static <T extends Comparable> void sort(T[] items){
         for(int i = 0; i < items.length; i++){
             int index = i;
             for(int j = i+1; j < items.length; j++){
-                if(items[j] < items[index]){
+                if(items[j].compareTo(items[index]) < 0){
                     index = j;
                 }
             }
             swap(items, index, i);
         }
-        return items;
     }
 
-    public static void swap(int[] items, int index, int i){
-        int temp = items[i];
+    public static <T> void swap(T[] items, int index, int i){
+        T temp = items[i];
         items[i] = items[index];
         items[index] = temp;
     };
--- src/test/java/SelectionSortTest.java
+++ src/test/java/SelectionSortTest.java
@@ -4,9 +4,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SelectionSortTest {
 
     @Test
-    public void sort() {
-        int[] items = {3,1,5,8,3,4,0,1,7};
+    public void sortInt() {
+        Comparable[] items = {3,1,5,8,3,4,0,1,7};
         SelectionSort.sort(items);
-        assertThat(items).isEqualTo(new int[]{0,1,1,3,3,4,5,7,8});
+        assertThat(items).isEqualTo(new Comparable[]{0,1,1,3,3,4,5,7,8});
+    }
+
+    public void sortString() {
+        Comparable[] items = {"asd", "hgo", "gg", "iko"};
+        SelectionSort.sort(items);
+        assertThat(items).isEqualTo(new Comparable[]{"asd", "iko", "gg", "hgo"});
     }
 }
